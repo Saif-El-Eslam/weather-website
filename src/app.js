8b2d6dd277859263eea9bcee5cd178b6dab398aa
Saif El-Eslam <saif82820@gmail.com>
2023-06-24 00:28:52 +0300

    setted up for deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forcast = require('./utils/forcast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const puplicPath = path.join(__dirname, '../public');
@@ -87,6 +88,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000.');
 });
